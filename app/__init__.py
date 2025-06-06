539ec26a7c1ad1bde6dab71fd5dd69603169ed43
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-06-06 18:02:59 -0300

    update

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,12 @@ from app.routes import routes
 from app.api import api
 from app_utils.db_models import db
 import os
+import json
+
+secrets_path = os.path.abspath(os.path.join(__file__, '..', '..', 'secrets.json'))
+with open(secrets_path) as f:
+    secrets = json.load(f)
+db_url = secrets.get('railway_db_url')
 
 root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 def create_app():
@@ -12,7 +18,7 @@ def create_app():
         static_folder=os.path.join(root_dir, 'static')
     )
     app.secret_key = 'secretkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:1234@localhost:3306/plataforma_eventos'
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
 
