770c7978d67b252608388e63ff0f9dac25ab685d
Cauã Oliveira <101019088+Caua-Oliveira@users.noreply.github.com>
2025-06-06 15:34:26 -0300

    Update rotas_helpers.py

--- app_utils/rotas_helpers.py
+++ app_utils/rotas_helpers.py
@@ -2,24 +2,24 @@ from flask import redirect, url_for, session, flash
 from functools import wraps
 from app_utils.db_models import Usuario
 
-def usuario_logado():
+def user_is_logged_in():
     uid = session.get('user_id')
     if not uid:
         return None
     return Usuario.query.get(uid)
 
-def necessita_login(f):
+def requires_login(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        if not usuario_logado():
+        if not user_is_logged_in():
             return redirect(url_for('login'))
         return f(*args, **kwargs)
     return decorated_function
 
-def necessita_admin(f):
+def requires_admin(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
-        user = usuario_logado()
+        user = user_is_logged_in()
         if not user or user.tipo.nome != 'admin':
             flash('Acesso negado.', 'danger')
             return redirect(url_for('inicio'))
